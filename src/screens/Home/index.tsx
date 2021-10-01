a4a6bdb57f8f559e6e05a47582c7c27cf97051cb
Rian de Almeida <rian223@outlook.com.br>
2021-10-01 11:32:47 -0400

    Home Interface

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
-import { View } from "react-native";
+import { View, FlatList } from "react-native";
 
 import { CategorySelect } from "../../components/CategorySelect";
+import { Appointment } from "../../components/Appointment";
+import { ListDivider } from "../../components/ListDivider";
+import { ListHeader } from "../../components/ListHeader";
 import { ButtonAdd } from "../../components/ButtonAdd";
 import { Profile } from "../../components/Profile";
 
@@ -9,6 +12,34 @@ import { styles } from "./styles";
 
 export function Home() {
   const [category, setCategory] = useState('');
+
+  const appointments = [
+    {
+      id: '1',
+      guild: {
+        id: '1',
+        name: 'Lendários',
+        icon: null,
+        owner: true
+      },
+      category: '1',
+      date: '01/10 às 10:00h',
+      description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
+    },
+    {
+      id: '2',
+      guild: {
+        id: '1',
+        name: 'Lendários',
+        icon: null,
+        owner: true
+      },
+      category: '1',
+      date: '01/10 às 10:00h',
+      description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
+    },
+    
+  ]
 
   function handleCategorySelect(categoryId: string) {
     categoryId === category ? setCategory('') : setCategory(categoryId);
@@ -21,10 +52,26 @@ export function Home() {
         <ButtonAdd />
       </View>
 
-      <View>
-        <CategorySelect
-          categorySelected={category}
-          setCategory={handleCategorySelect}
+      <CategorySelect
+        categorySelected={category}
+        setCategory={handleCategorySelect}
+      />
+
+      <View style={styles.content}>
+        <ListHeader
+          title="Partidas agendadas"
+          subtitle="Total 6"
+        />
+
+        <FlatList
+          data={appointments}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <Appointment data={item} />
+          )}
+          ItemSeparatorComponent={() => <ListDivider />}
+          style={styles.matches}
+          showsVerticalScrollIndicator={false}
         />
       </View>
     </View>
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import {
-  View,
-  Text,
-  Image,
+import { 
+  View, 
+  Text, 
+  Image  
 } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
@@ -11,36 +11,37 @@ import { styles } from './styles';
 
 import { ButtonIcon } from '../../components/ButtonIcon';
 
-export function SignIn() {
+export function SignIn(){
   const navigation = useNavigation();
 
-  function handleSignIn(){
-    navigation.navigate('Home')
+  function handleSignIn() {
+    navigation.navigate('Home');
   }
-  return (
-    <View style={styles.container}>
-      <Image
-        source={IllustrationImg}
-        style={styles.image}
+
+  return(
+    <View style={styles.container}>     
+      <Image 
+        source={IllustrationImg} 
+        style={styles.image} 
         resizeMode="stretch"
       />
 
       <View style={styles.content}>
         <Text style={styles.title}>
           Conecte-se {'\n'}
-          e organize suas {'\n'}
+          e organize suas {'\n'} 
           jogatinas
         </Text>
 
         <Text style={styles.subtitle}>
-          Crie grupos para jogar seus games {'\n'}
+          Crie grupos para jogar seus games {'\n'} 
           favoritos com seus amigos
         </Text>
 
-        <ButtonIcon
+        <ButtonIcon 
           title="Entrar com Discord"
           onPress={handleSignIn}
-        />
+        />                               
       </View>
     </View>
   );
