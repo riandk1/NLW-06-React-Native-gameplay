48b581f9ea77ed8504c4f61c89085dfd1b9228cf
Rian de Almeida <rian223@outlook.com.br>
2021-10-09 16:13:08 -0400

    User Auth and Context

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,23 +1,27 @@
 import React from "react";
 import { View, Text } from "react-native";
 
+import { useAuth } from "../../hooks/auth";
+
 import { Avatar } from "../Avatar";
 import { styles } from "./styles";
 
 export function Profile() {
+  const { user } = useAuth();
+  
   return (
     <View style={styles.container}>
 
-      <Avatar urlImage="https://github.com/riandk1.png"/>
+      <Avatar urlImage={user.avatar} />
 
       <View>
         <View style={styles.user}>
           <Text style={styles.greeting}>
-            Olá, 
+            Olá,
           </Text>
 
           <Text style={styles.username}>
-            Rian
+            {user.firstName}
           </Text>
         </View>
 
